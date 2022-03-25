b0aeee557b55af36e3b60d7e2eb2e0b855e552e4
igson <igson660@gmail.com>
2022-03-24 21:35:05 -0500

    add: configura rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import * as allData from './data'
+import { AppRoutes } from './shared/routes';
 
 export const App = () => {
   console.log(allData.data.data, allData.management.data[0].boards);
   return (
-    <div className="App">
-      hello
-    </div>
+    <AppRoutes />
   );
 }
