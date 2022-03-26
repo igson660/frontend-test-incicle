51981149097b9347d0e759753efae520204edc0c
igson <igson660@gmail.com>
2022-03-26 14:34:11 -0500

    add: estrutura do aside

---
+++ src/shared/components/aside/endormarketing/index.jsx
@@ -0,0 +1,33 @@
+import { Typography, Button } from '@mui/material';
+import { Box } from '@mui/system';
+
+export const Endormarketing = () => {
+  return(
+    <Box
+      width="21.5vw"
+      height="40vh"
+      bgcolor="#FFF2DE"
+      marginTop="37px"
+      marginBottom="20px"
+    >
+      <Typography
+        variant="h6"
+        component="header"
+        gutterBottom
+      >
+        Endormarketing
+      </Typography>
+      <Typography
+        variant="Body1"
+        gutterBottom
+      >
+        Endomarketing está relacionado às ações de treinamento ou qualificação dos colaboradores da empresa visando um melhor serviço para o cliente. Marketing interno, devido ao nome, é usualmente confundido com Endomarketing mesmo sendo conceitos diferentes.
+      </Typography>
+      <Button
+        variant="outlined"
+      >
+        Dispensar
+      </Button>
+    </Box>
+  );
+};
---
+++ src/shared/components/aside/painting/index.jsx
@@ -0,0 +1,21 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+
+export const Painting =() => {
+  return(
+    <Box
+      width="21.5vw"
+      height="40vh"
+      bgcolor="#FFFFFF"
+    >
+      <Typography
+        variant="h6"
+        component="header"
+        gutterBottom
+      >
+        Quadros de Gestão à Vista
+      </Typography>
+    </Box>
+  );
+};
